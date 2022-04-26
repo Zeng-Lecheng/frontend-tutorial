8f1a85bebd77ff840fa94db92f23dd27118aa3c5
Zeng-Lecheng <50188852+Zeng-Lecheng@users.noreply.github.com>
2022-04-25 21:36:20 -0400

    [Add](Home.js) num things

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,32 @@
-import {React} from "react";
+import { React, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, Text } from "react-native";
+import { Button } from "react-native-paper";
 
 export default function Home() {
+  const [num, setNum] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{flex: 1}}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={{flex: 1, justifyContent: "space-evenly", alignItems: "center"}}>
+        <Text>Num is {num}</Text>
+        <Button mode={"contained"} onPress={() => setNum(num + 1)}>
+          Increase num by 1.
+        </Button>
+        <Button mode={"contained"} onPress={() => setNum(num - 1)}>
+          Decrease num by 1.
+        </Button>
+        <Button mode={"contained"} onPress={() => setNum(0)}>
+          Reset num to 0.
+        </Button>
+        <Button mode={"contained"} onPress={() => setNum(num * 2)}>
+          Multiply num by 2.
+        </Button>
+        <Button mode={"contained"} onPress={() => setNum(num / 2)}>
+          Divide num by 2.
+        </Button>
+      </View>
     </View>
   );
 }
@@ -16,6 +36,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-around",
   },
 });
